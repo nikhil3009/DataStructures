618c63ab700db7c880c9652bd9431317cc7845fb
Sai Nikhil Reddy <sainikhilreddy@Sais-MacBook-Pro-2.local>
2022-10-08 08:52:12 -0400

    first commit

--- Factors.java
+++ Factors.java
@@ -1,3 +1,19 @@
 public class Factors {
-    public static void main
+    public static void main(String[] args){
+        System.out.println(factor(10));
+    }
+    public static int factor(int n){
+        int count = 0;
+        for(int i = 1; i*i<=n;i++){
+            if(n%i == 0){
+                if(i*i == n){
+                    count = count+1;
+                }
+                else{
+                    count = count+2;
+                }
+            }
+        }
+        return count;
+    }
 }
