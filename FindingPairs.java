e65f205e13402965becb5b56dfe2fc6dfbd5f55f
Sai Nikhil Reddy <sainikhilreddy@Sais-MacBook-Pro-2.local>
2022-10-19 12:44:03 -0400

    day4

--- FindingPairs.java
+++ FindingPairs.java
@@ -1,2 +1,23 @@
 public class FindingPairs {
+    public static void main(String[] args) {
+        int[] a = {5, 4, 1, 6, 10, 8, 9};
+        System.out.println(findPairs(a));
+    }
+    public static int findPairs(int[] a){
+        int count=0;
+        int k=10;
+        int n =a.length;
+        /*for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    if(a[i]+a[j]==k){
+                    count=count+1;
+                    }
+                }
+            }
+        }*/
+        //optimized solution
+
+        return count;
+    }
 }
