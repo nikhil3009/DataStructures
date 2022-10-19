de7d37593a78439e34c5bd1b0807e25e77d03156
Sai Nikhil Reddy <sainikhilreddy@Sais-MacBook-Pro-2.local>
2022-10-19 12:34:21 -0400

    day4

--- CountLargestNumberItself.java
+++ CountLargestNumberItself.java
@@ -1,20 +1,40 @@
 public class CountLargestNumberItself {
     public static void main(String[] args) {
-        int[] a={5,4,1,2,10,11,9};
+        int[] a = {5, 4, 1, 2, 10, 11, 9};
 
         System.out.println(largestNum(a));
     }
+    //static int largestNum(int[] a){
+    //  int count=0;
+    // int n = a.length;
+    // for(int i =0;i<n;i++){
+    //   for(int j=0;j<n;j++){
+    //       if(a[j]>a[i]){
+    //        count=count+1;
+    //      break;
+    //  }
+    //}
+    // }
+    // return count;
+    //}
+
+    //optimized solution is as follows.
     static int largestNum(int[] a){
-        int count=0;
+        int max=0;
         int n = a.length;
-        for(int i =0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(a[j]>a[i]){
-                    count=count+1;
-                    break;
-                }
+        for(int i=0;i<n;i++){
+            if(a[i]>max){
+                max=a[i];
             }
         }
-        return count;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(a[i]==max){
+                count=count+1;
+            }
+        }
+        int ans = n-count;
+        return ans;
     }
+
 }
