3e40fd6ecfc6133febdab874949cb11d718285d8
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2023-12-26 01:03:54 -0500

    medium array

--- Arrays/Medium/Leader.java
+++ Arrays/Medium/Leader.java
@@ -1,2 +1,30 @@
-package Arrays.Medium;public class Leader {
-}
+package Arrays.Medium;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Leader {
+    public static void main(String[] args) {
+        int n = 6;
+        int arr[] = {10, 22, 12, 3, 0, 6};
+        ArrayList<Integer> ans = printLeadersBruteForce(arr, n);
+        Collections.sort(ans, Collections.reverseOrder());
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+
+        public static ArrayList<Integer> printLeadersBruteForce ( int[] arr, int n){
+            ArrayList<Integer> ans = new ArrayList<>();
+            int max = arr[n - 1];
+            ans.add(max);
+            for (int i = n - 2; i >= 0; i--) {
+                if (arr[i] > max) {
+                    ans.add(arr[i]);
+                    max = arr[i];
+
+                }
+            }
+            return ans;
+        }
+    }
+
