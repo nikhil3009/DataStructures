dd5e8149d23f1a71429bf89a5c95a1801fcb8b7e
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-01-04 17:29:27 -0500

    solved-1Q

--- LeetCode_CodeNinjas/Leader.java
+++ LeetCode_CodeNinjas/Leader.java
@@ -1,2 +1,18 @@
-package LeetCode_CodeNinjas;public class Leader {
+package LeetCode_CodeNinjas;
+
+public class Leader {
+    public static List< Integer > superiorElements(int []a) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = a.length;
+        int max = a[n-1];
+        ans.add(max);
+        for(int i = n-2;i>=0;i--){
+            if(a[i]>max){
+                ans.add(a[i]);
+                max = a[i];           }
+        }
+        Collections.sort(ans);
+        return ans;
+
+    }
 }
