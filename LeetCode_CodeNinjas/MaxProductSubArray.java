3e24b3c51327960b2a9d3a5498239e1fba809c44
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-01-17 18:51:59 -0500

    MaxSubarrayProduct

---
+++ LeetCode_CodeNinjas/MaxProductSubArray.java
@@ -0,0 +1,19 @@
+package LeetCode_CodeNinjas;
+
+public class MaxProductSubArray {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int pre = 1;
+        int suf = 1;
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0;i<n;i++){
+            if(pre==0) pre =1;
+            if(suf == 0) suf =1;
+            pre *= nums[i];
+            suf *= nums[n-i-1];
+            ans = Math.max(ans,Math.max(pre,suf));
+        }
+        return ans;
+
+    }
+}
