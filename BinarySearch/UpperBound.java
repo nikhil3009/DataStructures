0a92d2cc8f2e03d878eebb1a8f76bba62c15a002
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-01-19 13:45:02 -0500

    UpperBound and insert position problem(2qs)

---
+++ BinarySearch/UpperBound.java
@@ -0,0 +1,20 @@
+package BinarySearch;
+
+public class UpperBound {
+    public static int upperBound(int []arr, int x, int n){
+        int start = 0;
+        int end = n-1;
+        int ans = n;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]>x){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+}
