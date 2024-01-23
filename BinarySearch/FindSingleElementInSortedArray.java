0a0a9f0b3ea426091ae04a1db371dfbc17b3e090
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-01-23 15:21:02 -0500

    Peak and single element

---
+++ BinarySearch/FindSingleElementInSortedArray.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+public class FindSingleElementInSortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int low = 1;
+        int high = n-2;
+        if(n==1){
+            return nums[0];
+        }
+        if(nums[0]!= nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!= nums[mid+1]){
+                return nums[mid];
+            }
+            if(mid%2 == 1 && nums[mid] == nums[mid-1] || mid%2 == 0 && nums[mid] == nums[mid+1]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+
+    }
+}
