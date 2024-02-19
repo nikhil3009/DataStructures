0155b082de55a218e60c5e84a24082e0774b79f0
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-02-19 15:12:47 -0500

    Subset-recursion

---
+++ Recursion/PrintSubSet.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class PrintSubSet {
+    List<List<Integer>> result = new ArrayList<>();
+    generateSubsets(nums,0,new ArrayList<>(),result);
+       return result;
+}
+    public void generateSubsets(int[] nums, int index, List<Integer> subset, List<List<Integer>> result){
+        if(index == nums.length){
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[index]);
+        generateSubsets(nums,index+1,subset,result);
+        subset.remove(subset.size()-1);
+        generateSubsets(nums,index+1,subset,result);
+
+    }
+}
