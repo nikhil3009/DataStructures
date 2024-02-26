337fe953d4b0f8da5de388a7bb78ac4c6256bb3e
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-02-26 15:08:02 -0500

    Parenthesis

---
+++ StacksandQueus/CheckParanthesis.java
@@ -0,0 +1,29 @@
+package StacksandQueus;
+
+public class CheckParanthesis {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c == '(' || c=='[' || c=='{'){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                else{
+                    char ch = st.pop();
+                    if(c == ')' &&  ch == '(' || c == '}' &&  ch == '{' || c == ']' &&  ch == '[' ) {
+                        continue;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return st.isEmpty();
+
+    }
+}
