d7f351922d696a061896c26c99149958e22e26d4
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-03-01 15:26:54 -0500

    Stockspan&dailytemperatures

---
+++ StacksandQueus/OnlineStockSpan.java
@@ -0,0 +1,18 @@
+package StacksandQueus;
+
+public class OnlineStockSpan {
+    Stack<int[]> st = new Stack<>();
+
+    public StockSpanner() {
+
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while(!st.isEmpty() && st.peek()[0] <= price ){
+            span += st.pop()[1];
+        }
+        st.push(new int[]{price,span});
+        return span;
+    }
+}
