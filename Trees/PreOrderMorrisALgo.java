4bdde2ab48af4eaaf325bb6114b1a0bcf815751d
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-03-15 13:42:24 -0400

    Morris Algorithm

---
+++ Trees/PreOrderMorrisALgo.java
@@ -0,0 +1,32 @@
+package Trees;
+
+public class PreOrderMorrisALgo {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right !=curr)
+                {
+                    prev = prev.right;
+                }
+                if(prev.right == null){
+                    prev.right = curr;
+                    ans.add(curr.val);
+                    curr = curr.left;
+                }
+                else{
+                    prev.right = null;
+
+                    curr = curr.right;
+                }
+            }
+        }
+        return ans;
+    }
+}
