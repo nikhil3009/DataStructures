aa36bdc2214ed7266df6456eab52fd506a19b813
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-04-08 17:30:50 -0400

    ShortestPath-UDG

---
+++ Graphs/SingleSourceShortestPath.java
@@ -0,0 +1,37 @@
+package Graphs;
+
+public class SingleSourceShortestPath {
+    public static int[] shortestPath(int n, int [][]edges, int src) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i =0;i<edges.length;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        int dist[] = new int[n];
+        for(int i =0 ;i<n;i++){
+            dist[i] = (int)1e9;
+        }
+        dist[src]= 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            for(int it:adj.get(node)){
+                if(dist[node]+1<dist[it]){
+                    dist[it] = dist[node]+1;
+                    q.add(it);
+                }
+            }
+        }
+        for(int i =0;i<n;i++){
+            if(dist[i] == 1e9){
+                dist[i] = -1;
+            }
+        }
+        return dist;
+    }
+}
