0ed849b4c48416846d513bafe4136ac435b0492b
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-04-23 00:14:02 -0400

    Leetcode 392,58,242

---
+++ Strings/IsSubSequence_392.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class IsSubSequence_392
+{
+    public boolean isSubsequence(String s, String t) {
+        int i =0;
+        int j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i +=1;
+            }
+            j += 1;
+        }
+
+        if(i == s.length()){
+            return true;
+        }
+        return false;
+    }
+}
